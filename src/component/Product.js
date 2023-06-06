7a53ea53e9b22928c68c6fb14b21f88ded24104f
Oluwakemi <oluwakemie05@gmail.com>
2023-06-06 08:37:41 +0100

    new commitr

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import { useState } from "react";
-import {} from "@heroicons/react/solid";
-import Currency from "react-currency-formatter";
+import {Currency} from "react-currency-formatter";
 
 const MAX_RATING = 5;
 const MIN_RATING = 1;
